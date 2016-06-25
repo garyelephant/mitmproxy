484cbb7a1bb192644520172c1c81f5226fcd6f94
Thomas Kriechbaumer <thomas@kriechbaumer.name>
2016-06-25 11:27:58 +0200

    update appveyor with tox

--- dev.bat
+++ dev.bat
@@ -5,6 +5,8 @@ virtualenv %VENV% --always-copy
 if %errorlevel% neq 0 exit /b %errorlevel%
 call %VENV%\Scripts\activate.bat
 if %errorlevel% neq 0 exit /b %errorlevel%
+pip install -U pip
+if %errorlevel% neq 0 exit /b %errorlevel%
 pip install -r requirements.txt
 if %errorlevel% neq 0 exit /b %errorlevel%
 
